4ea7e727c1b156c71eac3667728815cee9f4620f
Jon Penn <PennPD@sbcglobal.net>
2014-02-09 02:41:40 -0600

    Sun Feb  9 02:41:40 CST 2014

--- var/www/checkLive.php
+++ var/www/checkLive.php
@@ -1,5 +1,5 @@
 <?php
 	require_once('/usr/script/noCache.inc.php');
 	require_once('/usr/script/db.inc.php');
-	echo db1('SELECT contents FROM configuration WHERE var=\'live\'')->value;
+	echo db1('SELECT contents FROM configuration WHERE var=\'live\'')->contents;
 ?>
