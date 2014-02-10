0765d37eb3eef010543cffac9016c3f7af0cd76d
Jon Penn <PennPD@sbcglobal.net>
2014-02-09 23:39:42 -0600

    Sun Feb  9 23:39:42 CST 2014

--- usr/script/onDisconnect.php
+++ usr/script/onDisconnect.php
@@ -4,7 +4,7 @@
 	db0('UPDATE configuration SET contents=\'false\' WHERE var=\'live\'');
 	if(filesize('/tmp/dump.ogg') > 2 * 1024 * 1024) {
 		date_default_timezone_set('America/Chicago');
-		db0('INSERT INTO services(name) values (?)', date('F j, y A'));
+		db0('INSERT INTO services(name) values (?)', date('F j, Y A'));
 		$serviceId = db1('SELECT last_insert_rowid() AS id')->id;
 		db0('INSERT INTO resources (service, format, name) VALUES (?, 1, \'Sermon Recording\')', $serviceId);
 		$resourceId = db1('SELECT last_insert_rowid() AS id')->id;
